2101b1e0a3670bb4cbabc5d44bc1b6f3bdd85d6b
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-04-10 12:46:50 -0700

    styles contact and footer sections

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
 
+import '../../base-styles/base.scss';
 import './footer.scss';
 
 class Footer extends React.Component {
@@ -9,13 +9,10 @@ class Footer extends React.Component {
   render() {
     return (
       <>
-        <p>&copy; Becca Lee, 2019</p>
-        <a href="https://github.com/beccalee123/portfolio-site">Source Code</a>
-        <p>Add social links</p>
-        {/* <Link to='/'>About</Link> */}
-        {/* <Link>Portfolio</Link> */}
-        {/* <Link>Resume</Link> */}
-        {/* <Link>Contact</Link> */}
+        <footer>
+          <p>&copy; Becca Lee, 2019</p>
+          <a href="https://github.com/beccalee123/portfolio-site">Source Code</a>
+        </footer>
       </>
     );
   }
