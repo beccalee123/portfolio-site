2a63ab9c61aae8cdc9350096a801f3a73c6c8ec7
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-04-10 21:39:59 -0700

    fixes social icon link issue

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -13,10 +13,10 @@ class Contact extends React.Component {
           <h2>Get In Touch</h2>
           <p>Looking to add an ice-cream-loving dev to your team? Have a question about a project? I'd love to talk.</p>
           <ul>
-            <li><a href="mailto:becca@beccalee.dev"><img src={require('./assets/email.png')} alt="email icon"/></a></li>
-            <li><a href="https://twitter.com/the_becca_lee"><img src={require('./assets/twitter.png')} alt="twitter icon"/></a></li>
-            <li><a href="https://www.linkedin.com/in/beccalee123/"><img src={require('./assets/linkedin.png')} alt="LinkedIn icon"/></a></li>
-            <li><a href="https://github.com/beccalee123"><img src={require('./assets/github.png')} alt="GitHub icon"/></a></li>
+            <li><a href="mailto:becca@beccalee.dev" target="_blank" rel="noopener noreferrer"><img src={require('./assets/email.png')} alt="email icon"/></a></li>
+            <li><a href="https://twitter.com/the_becca_lee" target="_blank" rel="noopener noreferrer"><img src={require('./assets/twitter.png')} alt="twitter icon"/></a></li>
+            <li><a href="https://www.linkedin.com/in/beccalee123/" target="_blank" rel="noopener noreferrer"><img src={require('./assets/linkedin.png')} alt="LinkedIn icon"/></a></li>
+            <li><a href="https://github.com/beccalee123" target="_blank" rel="noopener noreferrer"><img src={require('./assets/github.png')} alt="GitHub icon"/></a></li>
             
           </ul>
         </section>
